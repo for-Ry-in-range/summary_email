9d901cf52a33ad27d91c8f81a182198746c6a163
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-07-18 13:37:07 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./features/landing/components/WarmReferralToolLandingPage/*.tsx"],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}
