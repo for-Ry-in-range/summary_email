13ae30228b1858cbe193c52e9b9806ff23dee1c4
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-07-29 14:23:47 -0400

    Update route.ts

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -3,6 +3,8 @@ import { inngest } from "../../../inngest/client";
 import {sendToAnth} from "../../../inngest/functions";
 import {sendSummary} from "../../../inngest/functions";
 
+export const maxDuration = 60;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
