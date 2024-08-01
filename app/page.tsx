d6f5dc1b9ea863515473199348fc10623517fe55
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-08-01 16:53:54 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn, 
+  SignedOut, 
+  UserButton,
+} from '@clerk/nextjs';
+import './globals.css';
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Label } from "@/components/ui/label"
+import React, { useState } from 'react';
+import { useUser } from '@clerk/clerk-react';
+import { MyComponent } from "./layout";
+
+
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-console.error("PRINTING SOMETHING")
-export default function Home() {
+export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ante id nulla viverra mollis. Etiam sed est quis augue ultrices lacinia. Etiam et elit et eros dictum auctor a et metus. Curabitur sodales est nec dolor hendrerit suscipit in sit amet quam. Sed id commodo mauris, fermentum tincidunt orci. Quisque efficitur erat ut nunc pellentesque, sed dignissim augue auctor. Quisque at dignissim tellus, sed lacinia nisi. Morbi nisl diam, aliquam vitae lacus et, egestas fermentum tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce at quam a nisi bibendum ultricies eget non nunc.</p>
-    </main>
+    <ClerkProvider>
+      <MyComponent />
+    </ClerkProvider>
   );
 }
