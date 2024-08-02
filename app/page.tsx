357a6d1919d8a27dbafdc0e920f9016b8aa8dd23
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-08-02 13:44:07 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import {
   ClerkProvider,
-  SignInButton,
-  SignedIn, 
-  SignedOut, 
-  UserButton,
 } from '@clerk/nextjs';
 import './globals.css';
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
-import { Label } from "@/components/ui/label"
 import React, { useState } from 'react';
-import { useUser } from '@clerk/clerk-react';
-import RootLayout from "./layout";
+import Home from "./layout";
 
 const clerk_pub_key = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API;
 
@@ -22,8 +13,8 @@ const prisma = new PrismaClient();
 
 export default function App() {
   return (
-    <ClerkProvider publishableKey={clerk_pub_key}>
-      <RootLayout/>
+    <ClerkProvider>
+      <Home/>
     </ClerkProvider>
   );
 }
