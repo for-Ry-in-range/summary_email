00cba8b20177152a64a9414e5ba448b4f6564a92
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-08-05 17:55:47 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
-"use client";
-import { inngest } from "./../inngest/client";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { useState } from "react";
-import { useUser } from "@clerk/nextjs";
 import {
   ClerkProvider,
   SignInButton,
@@ -12,10 +6,7 @@ import {
   SignedOut, 
   UserButton,
 } from '@clerk/nextjs';
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
-import { Label } from "@radix-ui/react-label";
-import { currentUser } from '@clerk/nextjs/server';
+import CreateInbox from "@/components/createInbox";
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
@@ -24,68 +15,27 @@ const inter = Inter({ subsets: ["latin"] });
 
 import { ReactNode } from "react";
 
-export default async function RootLayout() {
-  const [promptValue, setPromptValue] = useState('');
-  const [nameValue, setNameValue] = useState('');
-  const [showEmail, setShowEmail] = useState(false)
-  const user = await currentUser()
-
-  const createInbox = async (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    console.log("clicked")
-    if (user) {
-      setShowEmail(true)
-      try {
-        await inngest.send({
-          name: "myfunc/create-inbox",
-          data: {
-            name: nameValue,
-            prompt: promptValue,
-            send_to: user.primaryEmailAddressId
-          }
-        });
-      } catch(error) {
-        console.error("Error:", error)
-      }
-    }
-  }
-
-  const changeShowEmail = async (event: {preventDefault: () => void;}) => {
-    event.preventDefault();
-    
-  }
-
+export default function RootLayout({
+  children,
+}: {
+children: React.ReactNode
+}) {
   return (
-    <ClerkProvider>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+    <html>
+      <body>
+        <ClerkProvider>
+          <main className="flex min-h-screen flex-col items-center justify-between p-24">
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
 
-          <br/>
-
-          <h2>Create inbox</h2>
-          
-          <p className="leading-7 [&:not(:first-child)]:mt-6">Setup an inbox to process incoming emails</p>
-          
-          <Label>Name</Label> 
-          <Input placeholder="e.g. Investment newsletters inbox" value={nameValue} onChange={(e) => setNameValue(e.target.value)}/>
-
-          <br/>
-
-          <Label htmlFor="prompt">Extract command (prompt)</Label>
-          <Textarea id="prompt" value={promptValue} onChange={(e) => setPromptValue(e.target.value)}/>
-          <br/>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            This is the AI prompt that will be used to extract information from the incoming emails
-          </p>
-          <br/>
-          <button onClick={createInbox} className="bg-orange-500 hover:bg-orange-600">Create inbox</button>
-          {showEmail && <p>Forward emails to: 460d6ee3760a17630822+{}@cloudmailin.net</p>}
-      </main>
-    </ClerkProvider>
+              <CreateInbox/>
+          </main>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
