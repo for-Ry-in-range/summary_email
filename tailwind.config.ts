88cbb7290afb992f6e9c9cfc4e5968b51cede926
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-08-08 17:41:22 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,9 @@ const config = {
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    './@/**/*.{ts,tsx}',
 	],
   prefix: "",
   theme: {
